ad9252a28bff0fa99d7ae6da5be7e53948302564
Alex Trew <trew13690@gmail.com>
2020-05-11 12:22:13 -0700

    Custom Header

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,13 +1,16 @@
 import {createStackNavigator} from "react-navigation-stack";
 import Home from '../screens/home'
 import ReviewDetails from "../screens/reviewDetails";
+import Header from '../shared/header';
+import React from 'react'
 
 const screens = {
    Home: {
        screen: Home,
-       navigationOptions: {
-           title: 'GameZone',
-
+       navigationOptions: ({navigation}) => {
+          return {
+              headerTitle: () => <Header navigation={navigation} title='GameZone'/>,
+          }
        }
 
    },
